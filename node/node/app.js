ecc8f115749ee9e234e7dc90b09f375bcbbba480
13576487267 <heclrx@163.com>
2020-08-03 22:47:23 +0800

    aa

--- node/node/app.js
+++ node/node/app.js
@@ -4,26 +4,28 @@ const path = require('path');     // 引入path模块
 const url = require('url');     // 引入url模块
 const getMime = require('./moudle/common'); // 获取后缀名的自定义函数
 
-console.log(getMime('html'))
+getMime('html').then(res => {
+  console.log(res)
+})
 // 创建服务
 http.createServer(async (req, res) => {
   let pathname = url.parse(req.url).pathname; // 格式化获取的地址，去掉get传值
   pathname = pathname==='/'? '/index.html': pathname
   let extname = path.extname(pathname); // path的extname模块获取文件的后缀名
   if(pathname !== '/favicon.ico') {
-    // const result = await getMime(extname)
-    // console.log(result)
-    // fs.readFile('./web'+pathname, (err, data) => {
-    //   if(err) {
-    //     res.writeHead(200, {'Content-Type': `text/html;charset=utf-8`})
-    //     res.end('404页面')
-    //   }
-    //   let mime = getMain(extname)
-    //   res.writeHead(200, {'Content-Type': `${mime};charset=utf-8`})
-    //   res.end(data)
-    // }) 
+    const result = await getMime(extname)
+    console.log(result)
+    fs.readFile('./web'+pathname, (err, data) => {
+      if(err) {
+        res.writeHead(200, {'Content-Type': `text/html;charset=utf-8`})
+        res.end('404页面')
+      }
+      let mime = getMain(extname)
+      res.writeHead(200, {'Content-Type': `${mime};charset=utf-8`})
+      res.end(data)
+    }) 
   }
 }).listen(3001)
-// console.log(getMime('html'))
+// console.log(getMime('html')) 
 console.log(`http://localhost:3001`) 
 
--- node/node/moudle/common.js
+++ node/node/moudle/common.js
@@ -3,32 +3,25 @@
 const fs = require('fs')
 
 function getMime(extname) {
-  fs.readFile('../data/mime.json', (err, data) => {
-    if(err){
-      console.log(err)
-      return
-    }
-    let JSONDATA = JSON.parse(data.toString())
-    if(JSONDATA[extname]) {
-      console.log(JSONDATA[extname])
-    } else {
-    }
+  return new Promise((resolve, reject) => {
+    fs.readFile('../data/mime.json', (err, data) => {
+      if(err){
+        console.log(err)
+        reject(err)
+      }
+      console.log(data) 
+      let JSONDATA = JSON.parse(data.toString())
+      if(JSONDATA[extname]) {
+        resolve(JSONDATA[extname])
+      } else {
+        reject('json数据里没有该后缀文件')
+      }
+    })
   })
-  // return new Promise((resolve, reject) => {
-  //   fs.readFile('../data/mime.json', (err, data) => {
-  //     if(err){
-  //       console.log(err)
-  //       return
-  //     }
-  //     let JSONDATA = JSON.parse(data.toString())
-  //     if(JSONDATA[extname]) {
-  //       resolve(JSONDATA[extname])
-  //     } else {
-  //       reject('json数据里没有该后缀文件')
-  //     }
-  //   })
-  // })
 }
-getMime('html')
+
+// getMime('html').then(res => {
+//   console.log(res)
+// })
 
 module.exports = getMime
