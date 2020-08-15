9f1aae633e8e2338c74abb00ed169de15d637793
13576487267 <1186025162@qq.com>
2020-08-15 13:35:19 +0800

    111

---
+++ React/react-antd-demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+      <DatePicker />
+    </div>
+  );
+}
+
+export default App;
